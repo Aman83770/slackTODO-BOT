53181d33fe99af95b7d99a0d2d66f9b77da3b6de
Aman Kumar <aman83770@gmail.com>
2019-08-21 03:03:58 +0530

    removed console logs and formatted the responses

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -5,38 +5,108 @@ var mongoose = require('mongoose'),
 
 // get all tasks
 exports.getAllTaks = function(req, res) {
-  console.log(req.body);
-  Task.find({}, function(err, task) {
-    if (err)
+  Task.find({channel_id: req.body.channel_id}, function(err, task) {
+    if (err) {
       return res.send(err);
-    console.log(task);
-    let arr = [];
+    }
+
+    let text = "";
+    let i = 1;
     task.forEach(obj => {
-      if(obj.name) arr.push(obj.name);
-    })
-    return res.send(`${arr}`);
+      if(obj.name) text += i + ") " + obj.name + "\n";
+      i += 1;
+    });
+    
+    text = text === "" ? `No TODOs` : text;  
+    return res.send({ "response_type": "in_channel", "text": text });
   });
 };
 
 // create a todo
 exports.createTask = function(req, res) {
-  console.log(req.body);
-  const a = req.body;
-  a.name = a.text;
-  var newTask = new Task(a);
+  const data = req.body;
+
+  let final;
+  if (!data.text || data.text === "") {
+    final = {
+      // "response_type": "in_channel", // error should not be seen by all
+      "attachments": [
+        {
+            "color": "#FF4500",
+            "pretext": "An Error Happened",
+            "title": "Cannot add todo with empty message"
+        }
+      ]
+    }
+    return res.send(final);
+  }
+
+  data.name = data.text;
+  delete data.text;
+
+  var newTask = new Task(data);
   newTask.save((err, task) => {
-    if (err)
+    if (err) {
       return res.send(err);
-    return res.sendStatus(200);
+    }
+    final = {
+      "response_type": "in_channel",
+      "replace_original": true,
+      "text": "<@"+data.user_name+"> added " + "`"+data.name+"`"
+    }
+    return res.send(final);
   });
 };
 
 exports.deleteTask = function(req, res) {
-  Task.remove({
-    name: req.body.name
-  }, function(err, task) {
+  const data = req.body;
+  let final;
+
+  if (!data.text || data.text === "") {
+    final = {
+      // "response_type": "in_channel", // error should not be seen by all
+      "attachments": [
+        {
+            "color": "#FF4500",
+            "pretext": "An Error Happened",
+            "title": "Cannot delete todo with empty message/name"
+        }
+      ]
+    }
+    return res.send(final);
+  }
+
+  Task.deleteOne({
+    name: data.text,
+    channel_id: data.channel_id
+  }, function(err, result) {
     if (err)
       return res.send(err);
-    return res.json({ message: 'Task successfully deleted' });
+    // when there is no todo match
+    if (result.deletedCount < 1 ) {
+      final = {
+        // "response_type": "in_channel", // error should not be seen by all
+        "attachments": [
+          {
+              "color": "#FF4500",
+              "pretext": "An Error Happened",
+              "title": "No Todo found with this message"
+          }
+        ]
+      }
+      return res.send(final);
+    }
+
+    final = {
+      "response_type": "in_channel", // error should not be seen by all
+      "attachments": [
+        {
+            "color": "#008000",
+            "pretext": "TODO is removed from list",
+            "title": data.text
+        }
+      ]
+    }
+    return res.json(final);
   });
 };
